674bd00c207cbd31e4c6982d15460892fc721e99
hibachebbi <chebbihiba497@gmail.com>
2024-11-27 11:29:10 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,29 +1,21 @@
 <?php
-class config
-{
-    private static $pdo = null;
-
-    public static function getConnexion()
-    {
-        if (!isset(self::$pdo)) {
-            try {
-                self::$pdo = new PDO(
-                    'mysql:host=localhost;dbname=organic',
-                    'root',
-                    '',
-                    [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                    ]
-                );
-                //echo "connected successfully";
-            } catch (Exception $e) {
-                die('Erreur: ' . $e->getMessage());
-            }
-        }
-        return self::$pdo;
+class config {
+    private static $pdo = NULL;
+    
+    public static function getConnexion() {
+    if (!isset(self::$pdo)) {
+    try{
+    self::$pdo = new PDO('mysql:host=localhost;dbname=user', 'root', '',
+    [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    
+    }catch(Exception $e){
+    die('Erreur: '.$e->getMessage());
     }
-}
-
-config::getConnexion();
+    }
+    return self::$pdo;
+    }
+    }
 ?>
